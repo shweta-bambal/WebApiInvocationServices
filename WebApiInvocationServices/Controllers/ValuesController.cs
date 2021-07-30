9b201c7fd813d0b713cd35af1e637b35e3b282b9
Shweta Bambal <75642571+shweta-bambal@users.noreply.github.com>
2021-07-30 22:56:44 +0530

    Modified String values in Get Method

--- WebApiInvocationServices/Controllers/ValuesController.cs
+++ WebApiInvocationServices/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace WebApiInvocationServices.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value abc", "value 123" };
         }
 
         // GET api/<ValuesController>/5
